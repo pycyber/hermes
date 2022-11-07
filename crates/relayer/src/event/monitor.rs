f60633a6d384e37188ad4d97bac49eb506ed67e3
colmazia <ongart.pisan@gmail.com>
2022-11-07 14:37:44 +0700

    hermes can skip ignore_port_connection

--- crates/relayer/src/event/monitor.rs
+++ crates/relayer/src/event/monitor.rs
@@ -17,7 +17,9 @@ use tendermint_rpc::{
 };
 
 use ibc_relayer_types::{
-    core::ics02_client::height::Height, core::ics24_host::identifier::ChainId, events::IbcEvent,
+    core::ics02_client::height::Height,
+    core::ics24_host::identifier::{ChainId, PortChannelId},
+    events::IbcEvent,
 };
 
 use crate::{
@@ -101,6 +103,7 @@ pub struct EventMonitor {
     subscriptions: Box<SubscriptionStream>,
     /// Tokio runtime
     rt: Arc<TokioRuntime>,
+    ignore_port_channel: Vec<PortChannelId>,
 }
 
 // TODO: These are SDK specific, should be eventually moved.
@@ -153,6 +156,7 @@ impl EventMonitor {
         chain_id: ChainId,
         node_addr: Url,
         rt: Arc<TokioRuntime>,
+        ignore_port_channel: Vec<PortChannelId>,
     ) -> Result<(Self, EventReceiver, TxMonitorCmd)> {
         let (tx_batch, rx_batch) = channel::unbounded();
         let (tx_cmd, rx_cmd) = channel::unbounded();
@@ -180,6 +184,7 @@ impl EventMonitor {
             rx_cmd,
             node_addr,
             subscriptions: Box::new(futures::stream::empty()),
+            ignore_port_channel,
         };
 
         Ok((monitor, rx_batch, tx_cmd))
@@ -344,7 +349,11 @@ impl EventMonitor {
             core::mem::replace(&mut self.subscriptions, Box::new(futures::stream::empty()));
 
         // Convert the stream of RPC events into a stream of event batches.
-        let batches = stream_batches(subscriptions, self.chain_id.clone());
+        let batches = stream_batches(
+            subscriptions,
+            self.chain_id.clone(),
+            self.ignore_port_channel.clone(),
+        );
 
         // Needed to be able to poll the stream
         pin_mut!(batches);
@@ -442,8 +451,10 @@ impl EventMonitor {
 fn collect_events(
     chain_id: &ChainId,
     event: RpcEvent,
+    ignore_port_channel: &Vec<PortChannelId>,
 ) -> impl Stream<Item = Result<IbcEventWithHeight>> {
-    let events = crate::event::rpc::get_all_events(chain_id, event).unwrap_or_default();
+    let events =
+        crate::event::rpc::get_all_events(chain_id, event, ignore_port_channel).unwrap_or_default();
     stream::iter(events).map(Ok)
 }
 
@@ -451,12 +462,13 @@ fn collect_events(
 fn stream_batches(
     subscriptions: Box<SubscriptionStream>,
     chain_id: ChainId,
+    ignore_port_channel: Vec<PortChannelId>,
 ) -> impl Stream<Item = Result<EventBatch>> {
     let id = chain_id.clone();
 
     // Collect IBC events from each RPC event
     let events = subscriptions
-        .map_ok(move |rpc_event| collect_events(&id, rpc_event))
+        .map_ok(move |rpc_event| collect_events(&id, rpc_event, &ignore_port_channel))
         .map_err(Error::canceled_or_generic)
         .try_flatten();
 
